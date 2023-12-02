746eacd907b84cfd24ecdf319d98d9aef39e7f5b
Mirsait Sabirzyanov <mirsait77777@gmail.com>
2023-12-02 22:47:00 +0300

    Initial Commit

---
+++ UserInformation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UserInformation
+//
+//  Created by Мирсаит Сабирзянов on 02.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UserInformation/UserInformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserInformationApp.swift
+//  UserInformation
+//
+//  Created by Мирсаит Сабирзянов on 02.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct UserInformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
